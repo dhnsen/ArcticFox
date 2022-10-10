7a9e626ec6cee740961c7adbbc6da624318a88d2
Dan H <spag1024@hotmail.com>
2022-10-10 12:11:13 -0500

    Update PartList.java

--- app/src/main/java/com/bicycleshop/arcticfox/UI/PartList.java
+++ app/src/main/java/com/bicycleshop/arcticfox/UI/PartList.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.bicycleshop.arcticfox.Database.BicycleRepository;
+import com.bicycleshop.arcticfox.Entity.Product;
 import com.bicycleshop.arcticfox.R;
 
 public class PartList extends AppCompatActivity {
@@ -13,13 +15,15 @@ public class PartList extends AppCompatActivity {
     EditText editPrice;
     String name;
     Double price;
-
+    int productID;
+    BicycleRepository repo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_part_list);
         editName = findViewById(R.id.editProductName);
         editPrice = findViewById(R.id.editProductPrice);
+        productID = getIntent().getIntExtra("id", -1);
         name = getIntent().getStringExtra("name");
         price = getIntent().getDoubleExtra("price", -1.0);
         editName.setText(name);
@@ -28,6 +32,9 @@ public class PartList extends AppCompatActivity {
     }
 
     public void saveButton(View view) {
+        Product product;
+        if (productID == -1) {
 
+        }
     }
 }
